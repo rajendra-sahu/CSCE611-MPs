57d0c721a1d91265be994d73d4ca1b7b6e1b2f0a
rpsahu <rpsahu@tamu.edu>
2021-10-06 01:53:17 -0500

    Constructed the page table(compilation issues)

--- mp3/page_table.C
+++ mp3/page_table.C
@@ -10,32 +10,68 @@ ContFramePool * PageTable::kernel_mem_pool = NULL;
 ContFramePool * PageTable::process_mem_pool = NULL;
 unsigned long PageTable::shared_size = 0;
 
+//TODO Put meaingful asserts in the functions
 
 
 void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
+   //Inintializing global paramteters for the paging subsystem
+   kernel_mem_pool = _kernel_mem_pool;
+   process_mem_pool = _process_mem_pool;
+   shared_size = _shared_size;
+   
+   //assert(false);
    Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
+
+   page_directory = (unsigned long *)kernel_mem_pool->get_frames(1);            //Need only one frame for the page directory
+   
+   unsigned long *page_table = (unsigned long *)kernel_mem_pool->get_frames(1); //Since only first entry of PDE is valid.
+   
+   unsigned long address=0; // holds the physical address of where a page is
+   unsigned int i;
+
+   // map the first 4MB of memory
+   for(i=0; i<1024; i++)
+   {
+   	page_table[i] = address | 3;       // attribute set to: supervisor level, read/write, present(011 in binary)
+	address = address + 4096;          // 4096 = 4kb
+   }
+
+   // fill the first entry of the page directory
+   page_directory[0] = page_table;        // attribute set to: supervisor level, read/write, present(011 in binary)
+   page_directory[0] = page_directory[0] | 3;
+   
+   for(i=1; i<1024; i++)
+   {
+	page_directory[i] = 0 | 2;        // attribute set to: supervisor level, read/write, not present(010 in binary)
+   }
+
+
+   //assert(false);
    Console::puts("Constructed Page Table object\n");
 }
 
 
 void PageTable::load()
 {
-   assert(false);
+   current_page_table = this;
+   write_cr3(page_directory); // put that page directory address into CR3
+   
+   //assert(false);
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   assert(false);
+   write_cr0(read_cr0() | 0x80000000); // set the paging bit in CR0 to 1
+   
+   //assert(false);
    Console::puts("Enabled paging\n");
 }
 
